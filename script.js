1e63a068165af20b72007d38537b81d024bcf606
jamurguia91 <80859057+jamurguia91@users.noreply.github.com>
2022-05-25 09:56:05 -0700

    Got CircleType to run!!!
    Colin explained why mine wasnt working and we figured it out.
    order of library needs to be first.
    DONT use import, we arnt there and it requires alot of other things.

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 // my scripts
 
-// Load ES module
-import CircleType from `circletype`;
-// import CircleType from `circletype`;
-import * as circletype from "https://cdn.skypack.dev/circletype@2.3.0";
-
 // Instantiate `CircleType` with an HTML element.
 const circleType = new CircleType(document.getElementById('demo1'));
 
